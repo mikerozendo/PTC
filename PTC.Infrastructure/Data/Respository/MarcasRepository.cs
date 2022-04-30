a9109d80e2cafcbf4148bc53acf04e542157baaa
mikerozendo <mikerozendo@gmail.com>
2022-04-29 22:42:37 -0300

    Definindo setter no repository de marcas

--- PTC.Infrastructure/Data/Respository/MarcasRepository.cs
+++ PTC.Infrastructure/Data/Respository/MarcasRepository.cs
@@ -1,4 +1,8 @@
-﻿using PTC.Domain.Entities;
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using PTC.Domain.Entities;
+using PTC.Domain.Enums;
 using PTC.Domain.Interfaces.Repository;
 using PTC.Infrastructure.Data.Base;
 
@@ -21,9 +25,22 @@ namespace PTC.Infrastructure.Data.Respository
 
         public dynamic Inserir(Marca obj)
         {
+            List<Marca> marcas = new();
             AddParametro("Nome", obj.Nome);
-            ExecutarProcedure("P_MARCA_INSERIR");
-            return string.Empty;
+
+            var tabela = ExecutarProcedure("P_MARCA_INSERIR");
+
+            foreach (DataRow sdr in tabela.Rows)
+            {
+                marcas.Add(new Marca
+                {
+                    EnumSituacao = (EnumSituacao)sdr["Situacao"],
+                    Id = Convert.ToInt32(sdr["Id"]),
+                    Nome = sdr["Nome"].ToString()
+                });
+            }
+
+            return marcas;
         }
     }
 }
