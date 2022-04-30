c226268fc146e04ed326c8b2e68063cefd321b55
mikerozendo <mikerozendo@gmail.com>
2022-04-30 15:10:52 -0300

    Definindo repositorio de marcas

--- PTC.Infrastructure/Data/Respository/MarcasRepository.cs
+++ PTC.Infrastructure/Data/Respository/MarcasRepository.cs
@@ -10,6 +10,43 @@ namespace PTC.Infrastructure.Data.Respository
 {
     public class MarcasRepository : BaseRepository, IMarcasRepository
     {
+        public IEnumerable<Marca> ObterTodos()
+        {
+            List<Marca> marcas = new();
+            var tabela = ExecutarProcedure("P_MARCA_OBTER");
+
+            foreach (DataRow sdr in tabela.Rows)
+            {
+                marcas.Add(new()
+                {
+                    Id = Convert.ToInt32(sdr["Id"]),
+                    EnumSituacao = (EnumSituacao)sdr["Situacao"],
+                    Nome = sdr["Nome"].ToString()
+                });
+            }
+
+            return marcas;
+        }
+        public dynamic Inserir(Marca obj)
+        {
+            List<Marca> marcas = new();
+            AddParametro("Nome", obj.Nome);
+
+            var tabela = ExecutarProcedure("P_MARCA_INSERIR");
+
+            foreach (DataRow sdr in tabela.Rows)
+            {
+                marcas.Add(new()
+                {
+                    EnumSituacao = (EnumSituacao)sdr["Situacao"],
+                    Id = Convert.ToInt32(sdr["Id"]),
+                    Nome = sdr["Nome"].ToString()
+                });
+            }
+
+            return marcas;
+        }
+
         public void Alterar(Marca obj)
         {
             AddParametro("Nome", obj.Nome);
@@ -21,31 +58,6 @@ namespace PTC.Infrastructure.Data.Respository
         public void Deletar(Marca obj)
         {
             Alterar(obj);
-        }
-
-        public dynamic Inserir(Marca obj)
-        {
-            List<Marca> marcas = new();
-            AddParametro("Nome", obj.Nome);
-
-            var tabela = ExecutarProcedure("P_MARCA_INSERIR");
-
-            foreach (DataRow sdr in tabela.Rows)
-            {
-                marcas.Add(new Marca
-                {
-                    EnumSituacao = (EnumSituacao)sdr["Situacao"],
-                    Id = Convert.ToInt32(sdr["Id"]),
-                    Nome = sdr["Nome"].ToString()
-                });
-            }
-
-            return marcas;
-        }
-
-        public IEnumerable<Marca> ObterTodos()
-        {
-            throw new NotImplementedException();
         }
     }
 }
