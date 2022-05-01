4bd6f6f445543bb3f0f8c0a75b2bdbf8b0c713fb
mikerozendo <mikerozendo@gmail.com>
2022-04-30 21:56:00 -0300

    iniciando implementacao de repositorio de veiculos

--- PTC.Domain/Interfaces/Repository/IVeiculosRepository.cs
+++ PTC.Domain/Interfaces/Repository/IVeiculosRepository.cs
@@ -1,10 +1,5 @@
 ﻿using PTC.Domain.Entities;
 using PTC.Domain.Interfaces.Repository.CQRS;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PTC.Domain.Interfaces.Repository
 {
---
+++ PTC.Infrastructure/Data/Respository/VeiculosRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using PTC.Domain.Entities;
+using PTC.Domain.Enums;
+using PTC.Domain.Interfaces.Repository.CQRS;
+using PTC.Infrastructure.Data.Base;
+
+namespace PTC.Infrastructure.Data.Respository
+{
+    public class VeiculosRepository : BaseRepository, ICommandRepository<Veiculo>, IBaseQueryRepository<Veiculo>
+    {
+        public void Alterar(Veiculo obj)
+        {
+            AddParametro("ProprietarioId", obj.Proprietario.Id);
+            AddParametro("Renavam", obj.Renavam);
+            AddParametro("MarcaVeiculo", obj.MarcaVeiculo.Id);
+            AddParametro("Modelo", obj.Modelo);
+            AddParametro("DataFabricacao", obj.DataFabricacao);
+            AddParametro("Km", obj.Km);
+            AddParametro("Valor", obj.Valor);
+            AddParametro("Situacao", obj.EnumSituacao);
+            ExecutarProcedure("P_VEICULO_ALTERAR");
+        }
+
+        public dynamic Inserir(Veiculo obj)
+        {
+            AddParametro("ProprietarioId", obj.Proprietario.Id);
+            AddParametro("Renavam", obj.Renavam);
+            AddParametro("MarcaVeiculo", obj.MarcaVeiculo.Id);
+            AddParametro("Modelo", obj.Modelo);
+            AddParametro("DataFabricacao", obj.DataFabricacao);
+            AddParametro("Km", obj.Km);
+            AddParametro("Valor", obj.Valor);
+            AddParametro("Situacao", obj.EnumSituacao);
+            ExecutarProcedure("P_VEICULO_INSERIR");
+            return "Veiculo inserido com sucesso!";
+        }
+
+        public IEnumerable<Veiculo> ObterTodos()
+        {
+            List<Veiculo> veiculos = new();
+            var tabela = ExecutarProcedure("P_VEICULO_OBTER_TODOS");
+
+            foreach (DataRow sdr in tabela.Rows)
+            {
+                veiculos.Add(new()
+                {
+                    Id = Convert.ToInt32(sdr["Id"]),
+                    AnoModelo = Convert.ToDateTime(sdr["AnoModelo"]),
+                    Cadastro = Convert.ToDateTime(sdr["Cadastro"]),
+                    DataFabricacao = Convert.ToDateTime(sdr["DataFabricacao"]),
+                    Exclusao = Convert.ToDateTime(sdr["Exclusao"]),
+                    EnumSituacao = (EnumSituacao)sdr["Situacao"],
+                    Km = sdr["Km"].ToString(),
+                    Modelo = sdr["Modelo"].ToString(),
+                    Renavam = sdr["Renavam"].ToString(),
+                    Valor = Convert.ToDecimal(sdr["Valor"]),
+                    Proprietario = new()
+                    {
+                        Id = Convert.ToInt32(sdr["IdProprietario"]),
+                        Nome = sdr["NomeProprietario"].ToString(),
+                    },
+                    MarcaVeiculo = new()
+                    {
+                        Nome = sdr["NomeMarca"].ToString(),
+                        UrlImagem = sdr["CaminhoImagem"].ToString(),                       
+                    }
+                });
+            }
+
+            return veiculos;
+        }
+
+        public void Deletar(Veiculo obj)
+        {
+            return;
+        }
+    }
+}
