5459d893ee9709a4bb53e1aefbc57c5ac261853c
mikerozendo <mikerozendo@gmail.com>
2022-05-21 12:19:06 -0300

    Definindo novo enum p/ flow de negociacao de veiculo + nova prop com valor de revenda

--- PTC.Domain/Entities/Veiculo.cs
+++ PTC.Domain/Entities/Veiculo.cs
@@ -13,7 +13,8 @@ namespace PTC.Domain.Entities
         public DateTime DataFabricacao { get; set; }
         public DateTime AnoModelo { get; set; }
         public decimal Km { get; set; }
-        public decimal Valor { get; set; }
+        public decimal ValorCompra { get; set; }
+        public decimal ValorRevenda { get { return ValorCompra + (ValorCompra * 20 / 100); } }
         public EnumSituacao EnumSituacao { get; set; }
         public IFormFile Imagem { get; set; }
         public string CaminhoImagem { get; set; }
