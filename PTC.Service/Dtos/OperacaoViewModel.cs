87500b32460f9bf1fc254c93285acc3eee1bc34b
mikerozendo <mikerozendo@gmail.com>
2022-06-09 20:41:15 -0300

    criando primeiro dto e mapper do projeto

---
+++ PTC.Service/Dtos/OperacaoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace PTC.Application.Dtos
+{
+    public class OperacaoViewModel
+    {
+        public int ProprietarioId { get; set; }
+        public int CompradorId { get; set; }
+        public string RenavamVeiculo { get; set; }
+        public int MarcaVeiculoId { get; set; }
+        public string ModeloVeiculo { get; set; }
+        public DateTime DataFabricacaoVeiculo { get; set; }
+        public DateTime AnoModeloVeiculo { get; set; }
+        public decimal Km { get; set; }
+        public decimal ValorCompra { get; set; }
+        public decimal ValorTabela { get; set; }
+        public decimal ValorRevenda { get; set; }
+        public int SituacaoAquisicaoId { get; set; }
+        public int TipoPagamentoAquisicaoId { get; set; }
+        public int TipoPagamentoRevendaId { get; set; }
+        public DateTime? DataAquisicao { get; set; } = null;
+        public DateTime? DataRevenda { get; set; } = null;
+        public DateTime? CadastroVeiculo { get; set; } = null;
+        public DateTime? ExclusaoVeiculo { get; set; } = null;
+        public IFormFile Imagem { get; set; }
+        public string CaminhoImagem { get; set; }
+        public int VeiculoId { get; set; }
+    }
+}
---
+++ PTC.Service/Mapper/OperacaoMapper.cs
@@ -0,0 +1,48 @@
+﻿using PTC.Application.Dtos;
+using PTC.Domain.Entities;
+using PTC.Domain.Enums;
+
+namespace PTC.Application.Mapper
+{
+    public static class OperacaoMapper
+    {
+        public static Operacao ToDomain(OperacaoViewModel viewModel)
+        {
+            return new Operacao
+            {
+                Veiculo = new Veiculo
+                {
+                    AnoModelo = viewModel.AnoModeloVeiculo,
+                    Cadastro = viewModel.CadastroVeiculo,
+                    CaminhoImagem = viewModel.CaminhoImagem,
+                    DataFabricacao = viewModel.DataFabricacaoVeiculo,
+                    Exclusao = viewModel.DataFabricacaoVeiculo,
+                    Id = viewModel.VeiculoId,
+                    Km = viewModel.Km,
+                    ValorCompra = viewModel.ValorCompra,
+                    Modelo = viewModel.ModeloVeiculo,
+                    ValorRevenda = viewModel.ValorRevenda,
+                    ValorTabela = viewModel.ValorTabela,
+                    Renavam = viewModel.RenavamVeiculo,
+                    MarcaVeiculo = new Marca
+                    {
+                        Id = viewModel.MarcaVeiculoId
+                    }
+                },
+                Comprador = new Proprietario
+                {
+                    Id = viewModel.ProprietarioId,
+                },
+                Proprietario = new Proprietario
+                {
+                    Id = viewModel.CompradorId
+                },
+                EnumSituacaoAquisicao = (EnumSituacaoAquisicao)viewModel.SituacaoAquisicaoId,
+                EnumTipoPagamentoAquisicao = (EnumFormaPagamento)viewModel.TipoPagamentoAquisicaoId,
+                EnumTipoPagamentoRevenda = (EnumFormaPagamento)viewModel.TipoPagamentoRevendaId,
+                DataRevenda = viewModel.DataRevenda,
+                DataAquisicao = viewModel.DataAquisicao                
+            };
+        }
+    }
+}
