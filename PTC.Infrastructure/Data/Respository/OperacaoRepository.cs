2d39590f274c75efbd79eb2dc8a40d048dc6d4a7
mikerozendo <mikerozendo@gmail.com>
2022-06-25 23:34:36 -0300

    implementando leitura de retorno procedure obter operacoes

--- PTC.Infrastructure/Data/Respository/OperacaoRepository.cs
+++ PTC.Infrastructure/Data/Respository/OperacaoRepository.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using PTC.Domain.Enums;
 using PTC.Domain.Entities;
 using PTC.Infrastructure.Data.Base;
 using PTC.Domain.Interfaces.Repository;
@@ -35,6 +39,44 @@ namespace PTC.Infrastructure.Data.Respository
             AddParametro("CompradorId", obj.Comprador.Id);
             var tabela = await ExecutarProcedureAsync("P_OPERACAO_INSERIR");
             return tabela.Rows.Count;
+        }
+
+        public async Task<IEnumerable<Operacao>> ObterTodos()
+        {
+            List<Operacao> list = new();
+            var tabela = await ExecutarProcedureAsync("P_OPERACAO_OBTER");
+
+            foreach (DataRow sdr in tabela.Rows)
+            {
+                list.Add(new Operacao
+                {
+                    Id = (int)sdr["Id"],
+                    Cadastro = (DateTime)sdr["DataCadastro"],
+                    DataRevenda = (DateTime)sdr["DataRevendas"],
+                    EnumSituacaoAquisicao = (EnumSituacaoAquisicao)sdr["SituacaoAquisicao"],
+                    EnumTipoPagamentoAquisicao = (EnumFormaPagamento)sdr["TipoPagamentoAquisicao"],
+                    EnumTipoPagamentoRevenda = (EnumFormaPagamento)sdr["TipoPagamentoRevenda"],
+                    Proprietario = new Proprietario
+                    {
+                        Id = (int)sdr["ProprietarioId"],
+                        Nome = (string)sdr["NomeProprietarios"]
+                    },
+                    Veiculo = new Veiculo
+                    {
+                        Id = (int)sdr["VeiculoId"],
+                        Nome = (string)sdr["NomeVeiculo"],
+                        Modelo = (string)sdr["ModeloVeiculo"]
+                    },
+                    ValorCompra = (decimal)sdr["ValorCompra"],
+                    Comprador = new Proprietario
+                    {
+                        Id = (int)sdr["CompradorId"],
+                        Nome = (string)sdr["NomeCompradors"]
+                    },      
+                });
+            }
+
+            return list;
         }
     }
 }
